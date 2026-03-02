32ddad50e10decd621c0c99ab39481e520045f74
jacobs_matteo <matteo.jacobs@student.howest.be>
2026-03-02 11:23:15 +0100

    upgrade express server to support socket.io with client tracking and local IP logging

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+//Creates an express app
+//It wraps the express app in a Http server and attaches Socket.Io to that same server
+
+const express = require('express')
+const app = express()
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const os = require('os');
+const io = new Server(server);
+const port = process.env.PORT || 3000;
+
+
+const clients = {};
+
+// Creates a unique socket.id every time someone connects (desktop or phone)
+// unique socket.id gets storred in a clients object
+io.on("connection", (socket) => {
+  clients[socket.id] = { id: socket.id };
+  console.log("Socket connected", socket.id);
+
+  // When someone disconnects, remove them from the clients list.
+  socket.on("disconnect", () => {
+    delete clients[socket.id];
+  });
+});
 
 app.use(express.static("public"));
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+server.listen(port, () => {
+  const networkInterfaces = os.networkInterfaces();
+  for (const interfaceName in networkInterfaces) {
+    for (const iface of networkInterfaces[interfaceName]) {
+      if (iface.family === 'IPv4' && !iface.internal) {
+        console.log(`Link to project: http://${iface.address}:${port}`);
+      }
+    }
+  }
 });
