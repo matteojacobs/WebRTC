b5878754dad778fde53ba79bd92a6f483352a0eb
jacobs_matteo <matteo.jacobs@student.howest.be>
2026-03-09 14:51:06 +0100

    setup the signal server

--- index.js
+++ index.js
@@ -27,6 +27,21 @@ io.on("connection", (socket) => {
     io.emit("clients", clients);
   });
 
+  socket.on("peerOffer", (peerId, offer) => {
+    console.log(`Received peerOffer from ${socket.id} to ${peerId}`);
+    io.to(peerId).emit("peerOffer", peerId, offer, socket.id);
+  });
+
+  socket.on("peerAnswer", (peerId, answer) => {
+    console.log(`Received peerAnswer from ${socket.id} to ${peerId}`);
+    io.to(peerId).emit("peerAnswer", peerId, answer, socket.id);
+  });
+
+  socket.on("peerIce", (peerId, candidate) => {
+    console.log(`Received peerIce from ${socket.id} to ${peerId}`);
+    io.to(peerId).emit("peerIce", peerId, candidate, socket.id);
+  });
+
   //broadcast client list
   io.emit("clients", clients);
 });
